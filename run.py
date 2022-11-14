b8594a4a89e2f752468b860812b48aac3708d1f7
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-11-14 21:05:35 +0000

    add rock paper scissor game

--- run.py
+++ run.py
@@ -1,3 +1,33 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+print("Welcome to Rock paper scissor game")
+
+
+def playGame():
+
+    user_selection = input("Please select an option(rock, paper, scissor): ")
+
+    possible_choice = ["rock", "paper", "scissor"]
+    computer_selection = random.choice(possible_choice)
+
+    if user_selection == computer_selection:
+        print(f"\n You both chose {user_selection}, therefore its a tie")
+    elif user_selection == "rock":
+        if computer_selection == "scissor":
+            print(f"\n rock smashes scissor, therefore you win")
+        else:
+            print(f"\n Paper covers roc, you lose")
+    elif user_selection == "scissor":
+        if computer_selection == "paper":
+            print(f"\n Scissor cuts papaer, you win")
+        else:
+            print("\n Rock beats scissors, you lose")
+    elif user_selection == "paper":
+        if computer_selection == "rock":
+            print("\n paper covers rock, you win")
+        else:
+            print("\n scissor cuts paper, you lose")
+
+print(playGame())
+
+
