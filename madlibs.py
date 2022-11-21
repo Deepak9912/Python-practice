bfab51f286e847a8df2e9e85ca16e182ebb924f7
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-11-21 18:41:28 +0000

    add madlibs game

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+"""
+mablibs using string concetenation
+"""
+adj = input("Type Name of place, person or thing: ")
+verb = input("Type an activity: ")
+adj1 = input("Type Name of place, person or thing: ")
+noun = input("Type Name of place, person or thing: ")
+
+play = f"Hello my name is {adj}. I am {verb} at the moment. I have finished my course in {adj1} and now I am looking for a {noun}"
+
+print(play)
