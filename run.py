62f40a0af4e622ed4955d3312b8a26627e6ab49f
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-11-23 21:04:44 +0000

    add another game hangman

---
+++ run.py
@@ -0,0 +1,15 @@
+import random
+
+random_word = ["speak", "food", "education", "phone", "water", "computer"]
+
+#user_input = input("Please select a letter: ")
+computer_selection = random.choice(random_word)
+
+letters = len(computer_selection)*"_"
+
+
+def game():
+    user_inp = input("Please select a letter")
+
+    if user_inp = letters:
+        
